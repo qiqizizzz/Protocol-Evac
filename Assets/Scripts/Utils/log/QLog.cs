0935e20f6ab6df783a0b58fb0864249d5d9728b3
qiqizizzz <qiqizizzz@qq.com>
2026-07-11 23:09:49 +0800

    Init 导入QF框架 插件 文件结构初步

---
+++ Assets/Scripts/Utils/log/QLog.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: 客户端轻量日志封装，Release 构建自动抹除日志
+ * │  类    名: QLog.cs
+ * │  创    建: By qiqizizzz
+ * └──────────────────────────────────┘
+ */
+
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Utils.log
+{
+    public static class QLog
+    {
+        [Conditional("UNITY_EDITOR")]
+        public static void Info(object msg) => Debug.Log(msg);
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Warning(object msg) => Debug.LogWarning(msg);
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Error(object msg) => Debug.LogError(msg);
+    }
+}
